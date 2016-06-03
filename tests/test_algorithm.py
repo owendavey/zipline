83ea5c0271fb8e9e9dbc3a41f8936af1d39f397d
jfkirk <james.f.kirk@gmail.com>
2016-06-03 12:27:45 -0400

    TST: Finishes fixing fixes for the fixed fixtures

--- tests/test_algorithm.py
+++ tests/test_algorithm.py
@@ -1398,8 +1398,7 @@ class TestAlgoScript(WithLogger,
 
     @classmethod
     def make_daily_bar_data(cls):
-        days = len(cls.trading_schedule.execution_days_in_range(cls.START_DATE,
-                                                                cls.END_DATE))
+        days = len(cls.bcolz_daily_bar_days)
         return trades_by_sid_to_dfs(
             {
                 0: factory.create_trade_history(
@@ -1417,7 +1416,7 @@ class TestAlgoScript(WithLogger,
                     cls.sim_params,
                     cls.trading_schedule)
             },
-            index=cls.sim_params.trading_days,
+            index=cls.bcolz_daily_bar_days,
         )
 
     def test_noop(self):
